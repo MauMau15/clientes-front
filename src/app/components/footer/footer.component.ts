249b5e41c645a18859d0862c9e3550e796d4f9bc
Mauricio Renero <mauriciorenero@gmail.com>
2018-12-05 18:23:35 -0600

    adding footer

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  public autor:any;
+
+  constructor() { 
+    this.autor = {
+      nombre: "Mauricio Renero",
+      repo: "https://github.com/MauMau15/clientes-front"
+    };
+
+    console.log('footer works');
+    console.log(this.autor);
+  }
+
+  ngOnInit() {
+  }
+
+}
