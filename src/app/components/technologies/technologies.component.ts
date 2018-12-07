15c5e046ff04563210993215928cd45b2b80149f
Mauricio Renero <mauriciorenero@gmail.com>
2018-12-06 23:46:11 -0600

    modifying components.ts to suppres component.css files

--- src/app/components/technologies/technologies.component.ts
+++ src/app/components/technologies/technologies.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-technologies',
-  templateUrl: './technologies.component.html',
-  styleUrls: ['./technologies.component.css']
+  templateUrl: './technologies.component.html'
 })
 export class TechnologiesComponent implements OnInit {
 
