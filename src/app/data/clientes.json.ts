82364e6b587b10d01eae8f0a5b37e0573facfaa7
Mauricio Renero <mauriciorenero@gmail.com>
2018-12-07 01:16:54 -0600

    adding a new fake cliente

--- src/app/data/clientes.json.ts
+++ src/app/data/clientes.json.ts
@@ -7,11 +7,16 @@ export const CLIENTES: Cliente[]=[
         apellido:'Renero',
         createAt:'15-06-95',
         email:'mau@mail.com'
-    },
-    {
+    },{
         id: 2,
         nombre: 'Javi',
         apellido:'Renero',
+        createAt:'15-06-95',
+        email:'mau@mail.com'
+    },{
+        id: 3,
+        nombre: 'Hola',
+        apellido:'Jello',
         createAt:'15-06-95',
         email:'mau@mail.com'
     }
