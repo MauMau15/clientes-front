a190605f2662ea1f5ac6285a384966c88bbf5c13
Mauricio Renero <mauriciorenero@gmail.com>
2018-12-07 00:11:55 -0600

    adding clientes json fake

--- src/app/classes/clientes.ts
+++
@@ -1,7 +0,0 @@
-export class Clientes{
-    id: number;
-    nombre:string;
-    apellido:string;
-    createAt:string;
-    email:string;
-}
--- src/app/components/clientes/clientes.component.ts
+++ src/app/components/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../../classes/Cliente'
 
 @Component({
   selector: 'app-clientes',
@@ -6,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesComponent implements OnInit {
 
+  clientes: Cliente[] = [
+    {
+      id: 1,
+      nombre:'Mau',
+      apellido:'Renero',
+      createAt:'15-06-95',
+      email:'mau@mail.com'
+    },
+    {
+      id: 2,
+      nombre: 'Javi',
+      apellido:'Renero',
+      createAt:'15-06-95',
+      email:'mau@mail.com'
+    }
+  ];
+  
   constructor() { }
 
   ngOnInit() {
