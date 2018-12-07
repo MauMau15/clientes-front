aaaa963ce69b56d3152f6c594b9f51684e9f86a2
Mauricio Renero <mauriciorenero@gmail.com>
2018-12-07 00:56:42 -0600

    adding fake data

---
+++ src/app/data/clientes.json.ts
@@ -0,0 +1,18 @@
+import { Cliente } from "../classes/Cliente";
+
+export const CLIENTES: Cliente[]=[
+    {
+        id: 1,
+        nombre:'Mau',
+        apellido:'Renero',
+        createAt:'15-06-95',
+        email:'mau@mail.com'
+    },
+    {
+        id: 2,
+        nombre: 'Javi',
+        apellido:'Renero',
+        createAt:'15-06-95',
+        email:'mau@mail.com'
+    }
+];
