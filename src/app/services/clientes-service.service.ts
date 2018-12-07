c1a07f84532bae732d06a9838f38bfc01a571d30
Mauricio Renero <mauriciorenero@gmail.com>
2018-12-07 01:17:53 -0600

    returning fake data syncly

--- src/app/services/clientes-service.service.ts
+++ src/app/services/clientes-service.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
-
+import { CLIENTES } from '../data/clientes.json';
+import { Cliente } from '../classes/Cliente';
 @Injectable({
   providedIn: 'root'
 })
 export class ClientesServiceService {
 
-  constructor() { }
+  clientes:Cliente[] =  CLIENTES;
+
+  constructor() { 
+    console.log('servicio Cliente listo');
+  }
+
+  getClientes():Cliente[]{
+    return this.clientes;
+    console.log(this.clientes);
+  }
 }
