ce55443eb9115d156583c1f1e19bd226b805fa2c
Mauricio Renero <mauriciorenero@gmail.com>
2018-12-07 00:28:05 -0600

    adding clientes route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
 import { TechnologiesComponent } from './components/technologies/technologies.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
 
 const APP_ROUTES: Routes = [
     { path: 'Technologies', component: TechnologiesComponent },
-    { path: '**', pathMatch: 'full', redirectTo: 'Technologies' }
+    { path: 'Clientes', component: ClientesComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'Clientes' }
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES,{useHash:true});
